facb6c5a06f6c48923352b8133de96bf6ca90011
JUSTFIDEL <fidenki1429@gmail.com>
2023-04-01 19:57:59 +0100

    git ignored

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
 import { legacy_createStore as createStore } from 'redux'
 
 const reducer = (state = { counter: 0 }, action) => {
+	if (action.type === 'INC') {
+		return { counter: state.counter + 1 }
+	}
+
+	if (action.type === 'DCR') {
+		return { counter: state.counter - 1 }
+	}
+
 	return state
 }
 
